6abd66ae6c3d449c6b867f78006daea869e8cd86
Guo Yu <i@guoyu.me>
2013-09-15 03:19:29 +0800

    add demo

---
+++ demo/index.js
@@ -0,0 +1,23 @@
+var API = require('../index');
+
+// init a API
+var demo = new API({
+    read: {
+        url: '/demo/read/{{name}}',
+        method: 'get'
+    },
+    update: {
+        url: '/demo/update',
+        method: 'post'
+    }
+},{
+    server: 'http://abc.com'
+});
+
+// run demo
+demo.read({
+    name: 123
+},function(err, result){
+    // console.log(err);
+    // console.log(result.response);
+});
