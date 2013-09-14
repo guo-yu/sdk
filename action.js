d97fb8a67d7a95ad22d26b2bde98ed2a4313ecea
Guo Yu <i@guoyu.me>
2013-09-15 03:18:55 +0800

    delete console.log

--- action.js
+++ action.js
@@ -4,7 +4,6 @@ var api = require('beer'),
 module.exports = function(route, self) {
     return function(params, cb) {
         var parent = self.parent;
-        console.log(parent.server + urlmaker(params, route));
         api[route.method](parent.server + urlmaker(params, route), params, function(err, result) {
             cb(err, result);
         });
