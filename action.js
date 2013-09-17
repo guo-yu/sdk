2f5a5daa3fef40ae7b62ffbed1583c976c5ac58f
Guo Yu <i@guoyu.me>
2013-09-17 18:19:09 +0800

    ship to 0.0.2

--- action.js
+++ action.js
@@ -1,10 +1,13 @@
 var api = require('beer'),
-    urlmaker = require('./url');
+    urlmaker = require('./url'),
+    _ = require('underscore');
 
 module.exports = function(route, self) {
     return function(params, cb) {
-        var parent = self.parent;
-        api[route.method](parent.server + urlmaker(params, route), params, function(err, result) {
+        var parent = self.parent,
+            method = route.method ? route.method : 'get';
+        if (route.headers && _.isObject(route.headers)) params.headers = route.headers;
+        api[method](parent.server + urlmaker(params, route), params, function(err, result) {
             cb(err, result);
         });
     }
