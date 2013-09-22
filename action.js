38013da22e867405201bf16b298a34d9caaed21a
Guo Yu <i@guoyu.me>
2013-09-22 14:55:03 +0800

    ship to 0.0.5

--- action.js
+++ action.js
@@ -6,10 +6,11 @@ module.exports = function(route, self) {
     return function(params, cb) {
         var parent = self.parent,
             method = route.method ? route.method : 'get',
-            vars = params;
-        if (route.headers && _.isObject(route.headers)) params.headers = route.headers;
+            raw = (params && _.isObject(params)) ? params: {},
+            vars = raw;
         if (method === 'get') vars = params.query ? params.query : {};
-        api[method](parent.server + urlmaker(params, route), vars, function(err, result) {
+        if (route.headers && _.isObject(route.headers)) vars.headers = route.headers;
+        api[method](parent.server + urlmaker(raw, route), vars, function(err, result) {
             cb(err, result, parent);
         });
     }
