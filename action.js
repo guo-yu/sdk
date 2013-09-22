81f63831bdd718f069670daea2e93c39d3af4252
Guo Yu <i@guoyu.me>
2013-09-22 15:06:16 +0800

    bugs fixed

--- action.js
+++ action.js
@@ -1,17 +1,18 @@
 var api = require('beer'),
-    urlmaker = require('./url'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    urlmaker = require('./url');
 
 module.exports = function(route, self) {
     return function(params, cb) {
         var parent = self.parent,
             method = route.method ? route.method : 'get',
+            callback = (_.isFunction(params) && !cb) ? params : cb,
             raw = (params && _.isObject(params)) ? params: {},
             vars = raw;
         if (method === 'get') vars = params.query ? params.query : {};
         if (route.headers && _.isObject(route.headers)) vars.headers = route.headers;
         api[method](parent.server + urlmaker(raw, route), vars, function(err, result) {
-            cb(err, result, parent);
+            callback(err, result, parent);
         });
     }
 }
