56fa42048acf69b3f89cc564c68d0c3066bb5784
Guo Yu <i@guoyu.me>
2014-04-14 00:56:26 +0800

    0.0.9: made callback configable

--- libs/action.js
+++ libs/action.js
@@ -3,11 +3,18 @@ var _ = require('underscore');
 var request = require('request');
 var urlmaker = require('./url');
 
+function defaultCallback(err, response, body, next) {
+  if (err) return next(err);
+  var code = response.statusCode;
+  if (code !== 200) return next(new Error(code));
+  return next(null, response, body);
+}
+
 module.exports = function(host, route, rules) {
 
   return function(params, callback) {
-    var method = route.method.toLowerCase() || 'get';
-    var cb = (_.isFunction(params) && !callback) ? params : callback;
+    var method = route.method ? route.method.toLowerCase() : 'get';
+    var done = (_.isFunction(params) && !callback) ? params : callback;
     var options = (params && _.isObject(params)) ? params : {};
 
     if (rules) {
@@ -18,7 +25,14 @@ module.exports = function(host, route, rules) {
     options.method = method;
     options.url = url.resolve(host, urlmaker(route.url, options));
 
-    return request(options, cb);
+    return request(options, function(err, response, body) {
+      return defaultCallback(err, response, body, function(err, res, body) {
+        if (route.callback && _.isFunction(route.callback)) {
+          return route.callback(err, res, body, done);
+        }
+        return done(err, res, body);
+      });
+    });
   };
 
 };
